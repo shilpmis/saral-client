283e101537fb48a2191f46dba58b4d665b047ab4
HarshPanchal26 <hrppanchal27@gmail.com>
2025-02-10 23:10:47 +0530

    added verify service for user authentication

--- src/components/Routes/index.routes.tsx
+++ src/components/Routes/index.routes.tsx
@@ -19,47 +19,61 @@ import { useAppSelector } from "@/redux/hooks/useAppSelector"
 import { selectIsAuthenticated } from "@/redux/slices/authSlice"
 import PrivateRoute from "./private.routes"
 import { useEffect } from "react"
+import { useVerifyQuery } from "@/services/AuthService"
 
 export default function RootRoute() {
+
   const isAuthenticated = useAppSelector(selectIsAuthenticated)
 
-  useEffect(()=> {
-   console.log("isAuthenticated", isAuthenticated)
+  const { data, error, isLoading, isFetching, isSuccess, isError } = useVerifyQuery()
+
+  useEffect(() => {
+
   }, [isAuthenticated])
-  
+
   return (
-    <Router>
-      <Routes>
-        {/* <Route
+    <>
+      {isLoading && <p>Loading...</p>}
+      {isFetching && <p>Fetching...</p>}
+      {isSuccess && <Router>
+        <Routes>
+          {/* <Route
           path="/"
           element={isAuthenticated ? <Navigate to="/d/staff" replace /> : <Navigate to="/auth/login" replace />}
         /> */}
 
-        <Route path="/auth" element={<AuthLayout />}>
-          <Route path="login" element={<Login />} />
-        </Route>
+          <Route path='/' element={<Login />}></Route>
 
-        <Route path="/d" >
-          <Route element={<AdminLayout />}>
-            <Route path="students" element={<Students />} />
-            {/* <Route path="student/add" element={<StudentForm />} />
+          <Route path="/auth" element={<AuthLayout />}>
+            <Route path="login" element={<Login />} />
+          </Route>
+
+          <Route path="/d" >
+            <Route element={<AdminLayout />}>
+
+              <Route path="students" element={<Students />} />
+              {/* <Route path="student/add" element={<StudentForm />} />
             <Route path="student/edit/:id" element={<StudentForm />} /> */}
-            <Route path="staff" element={<Staff />} />
-            <Route path="payroll" element={<Payroll />} />
-            <Route path="fee" element={<Fees />} />
-            <Route path="user-management" element={<UserManagement />} />
 
-            <Route path="settings" element={<SettingsPage />}>
-              <Route path="general" element={<GeneralSettings />} />
-              <Route path="academic" element={<AcademicSettings />} />
-              <Route path="staff" element={<StaffSettings />} />
-              <Route path="payroll" element={<PayrollSettings />} />
-              <Route path="fees" element={<FeesSettings />} />
+              <Route path="staff" element={<Staff />} />
+              <Route path="payroll" element={<Payroll />} />
+
+              <Route path="fee" element={<Fees />} />
+
+              <Route path="user-management" element={<UserManagement />} />
+
+              <Route path="settings" element={<SettingsPage />}>
+                <Route path="general" element={<GeneralSettings />} />
+                <Route path="academic" element={<AcademicSettings />} />
+                <Route path="staff" element={<StaffSettings />} />
+                <Route path="payroll" element={<PayrollSettings />} />
+                <Route path="fees" element={<FeesSettings />} />
+              </Route>
             </Route>
           </Route>
-        </Route>
-      </Routes>
-    </Router>
+        </Routes>
+      </Router>}
+    </>
   )
 }
 
