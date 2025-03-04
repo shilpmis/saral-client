dfb38e1f97eddc1b0608a953960db03369d4f633
Bhavik1363 <bhavik1363@gmail.com>
2025-03-04 13:22:59 +0530

    PWA creation + serverurl handling through environment mode

--- src/utils/base-urls.ts
+++ src/utils/base-urls.ts
@@ -1,5 +1,22 @@
+let serverBaseUrl = '';
+switch (__Mode__) {
+  case 'production':
+    serverBaseUrl = "https://saral-server-dev.melzo.com/"
+    break;
+
+  case 'development':
+    serverBaseUrl = "https://saral-server-dev.melzo.com/"
+    break;
+
+  case 'localdb':
+    serverBaseUrl = "http://localhost:3333/"
+    break;
+
+  default:
+    serverBaseUrl = "https://saral-server-dev.melzo.com/"
+    break;
+}
 const baseUrl = {
-  serverUrl: "http://localhost:3333/",
-  localServerUrl: "http://localhost:3333/"
+  serverUrl: serverBaseUrl
 };
 export default baseUrl;
