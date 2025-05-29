097fa8200e2bbca80f412d4f3db6d55290028043
HarshPanchal26 <hrppanchal27@gmail.com>
2025-05-29 17:15:00 +0530

    fix typo for api call related to user management

--- src/services/UserManagementService.ts
+++ src/services/UserManagementService.ts
@@ -63,7 +63,7 @@ export const UserManagementApi = createApi({
       { page: number; school_id: number; academic_sessions: number }
     >({
       query: ({ page, academic_sessions }) => ({
-        url: `staff/?type=non-activeuser&academic_sessions=${academic_sessions}&page=${page}`,
+        url: `staff?type=non-activeuser&academic_sessions=${academic_sessions}&page=${page}&alldata=${true}`,
         method: "GET",
       }),
     }),
